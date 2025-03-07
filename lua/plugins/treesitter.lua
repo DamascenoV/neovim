bc4facd4366f9c7555c907520a46807f27840eb4
DamascenoV <victor.c.damasceno@gmail.com>
2025-03-07 23:42:49 +0000

    Update Deps

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
   version = false,
-  event = {"BufReadPost", "BufWritePost", "BufNewFile"},
+  event = { "BufReadPost", "BufWritePost", "BufNewFile" },
   config = function()
     require('nvim-treesitter.configs').setup({
       ensure_installed = {
