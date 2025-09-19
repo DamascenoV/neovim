9f283991344482a30012e67d177c924c64d6eda6
DamascenoV <victor.c.damasceno@gmail.com>
2025-09-19 11:33:10 +0100

    Revert to tree-sitter to master

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,40 +1,36 @@
 MiniDeps.later(function()
   MiniDeps.add({
     source = 'nvim-treesitter/nvim-treesitter',
-    checkout = 'main',
-    hooks = {
-      post_checkout = function() vim.cmd('TSUpdate') end,
-      post_install = function()
-        require('nvim-treesitter.configs').setup({
-          ensure_installed = {
-            'go',
-            'lua',
-            'typescript',
-            'javascript',
-            'css',
-            'vim',
-            'php',
-            'vue',
-            'markdown',
-            'markdown_inline',
-            'elixir',
-            'heex',
-            'zig',
-          },
-          auto_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },
-          incremental_selection = {
-            enable = true,
-            keymaps = {
-              init_selection = '<c-p>',
-              node_incremental = '<c-p>',
-              scope_incremental = '<c-s>',
-              node_decremental = '<c-y>',
-            },
-          },
-        })
-      end
+    hooks = {post_checkout = function() vim.cmd('TSUpdate') end,},
+  })
+
+  require('nvim-treesitter.configs').setup({
+    ensure_installed = {
+      'go',
+      'lua',
+      'typescript',
+      'javascript',
+      'css',
+      'vim',
+      'php',
+      'vue',
+      'markdown',
+      'markdown_inline',
+      'elixir',
+      'heex',
+      'zig',
+    },
+    auto_install = false,
+    highlight = { enable = true },
+    indent = { enable = true },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<c-p>',
+        node_incremental = '<c-p>',
+        scope_incremental = '<c-s>',
+        node_decremental = '<c-y>',
+      },
     },
   })
 end)
