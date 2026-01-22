9dcdebef37d21bccd30c976ee43bcd0fcd515556
DamascenoV <victor.c.damasceno@gmail.com>
2026-01-22 18:39:13 +0000

    Up

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -23,12 +23,14 @@ MiniDeps.later(function()
 
   require('nvim-treesitter').install(languages)
 
-  local new_autocmd = function(event, pattern, callback, desc)
-    local opts = { pattern = pattern, callback = callback, desc = desc }
-    vim.api.nvim_create_autocmd(event, opts)
-  end
-
   local filetypes = vim.iter(languages):map(vim.treesitter.language.get_filetypes):flatten():totable()
-  local ts_start = function(ev) vim.treesitter.start(ev.buf) end
-  new_autocmd('FileType', filetypes, ts_start, 'Ensure enabled tree-sitter')
+
+  vim.api.nvim_create_autocmd(
+    'FileType',
+    {
+      pattern = filetypes,
+      callback = function(ev) vim.treesitter.start(ev.buf) end,
+      desc = 'Ensure enabled tree-sitter'
+    }
+  )
 end)
