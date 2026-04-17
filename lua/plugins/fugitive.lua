f9e6212abf7851eb20f65590c45afe2710d8a909
DamascenoV <victor.c.damasceno@gmail.com>
2026-04-17 17:43:56 +0100

    Add keymaps to fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,1 +1,4 @@
 vim.pack.add({ "https://github.com/tpope/vim-fugitive" })
+
+vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>Git<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>gd", "<cmd>Git diff<CR>", { noremap = true, silent = true })
