b3f190c7fab177d02c1527513b56eaa5b1002c3e
DamascenoV <victor.c.damasceno@gmail.com>
2026-04-21 11:02:43 +0100

    Fugitive: git diff view

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,3 +2,4 @@ vim.pack.add({ "https://github.com/tpope/vim-fugitive" })
 
 vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>Git<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>gd", "<cmd>Git diff<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>gv", "<cmd>Gdiffsplit<CR>", { noremap = true, silent = true })
