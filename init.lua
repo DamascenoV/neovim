0efec7626ee02680e75b64847b9471547e2533e4
DamascenoV <victor.c.damasceno@gmail.com>
2026-04-22 15:33:53 +0100

    Remove auto-loading plugins.
    I only have two plugins... this is not needed

--- init.lua
+++ init.lua
@@ -1,15 +1,12 @@
+-- config
 require('config.options')
 require('config.keymaps')
 require('config.autocmds')
 require('config.lsp')
 
-local function require_directory(path)
-  local files = vim.fn.readdir(vim.fn.stdpath('config') .. '/lua/' .. path)
-  for _, file in ipairs(files) do
-    local plugin_name, _ = file:gsub('.lua', '')
-    require(path .. '.' .. plugin_name)
-  end
-end
+-- plugins
+require('plugins.treesitter')
+require('plugins.fugitive')
 
-require_directory('plugins')
+-- custom
 require('util.compile')
