b892b80244d34330998c31b85d85255bfd37d3d2
DamascenoV <victor.c.damasceno@gmail.com>
2026-05-14 09:04:20 +0100

    keymap: pack update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -56,6 +56,7 @@ end, { desc = 'Project [Q]uick Diagnostics' })
 keymap('n', '<leader>s', '<cmd>cwindow<CR>', { desc = '[Q]uick List' })
 keymap('n', '<leader>ff', ':find ', { desc = '[F]ind [F]iles' })
 keymap('n', '<leader>fg', ':grep ', { desc = '[F]ind [G]rep' })
+keymap('n', '<leader>pu', vim.pack.update, { desc = '[P]ack [U]pdate' })
 keymap('n', '<C-e>', '<cmd>Ex<CR>', { desc = '[E]xplorer' })
 
 keymap('n', '<leader>cc', ':Compile ')
