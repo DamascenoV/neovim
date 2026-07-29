ab02fa0b97763d2ee464ed6cd4bd3d5f255e8465
DamascenoV <victor.c.damasceno@gmail.com>
2026-07-29 11:56:05 +0100

    test

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,5 +1,21 @@
-vim.pack.add({ 'https://github.com/tpope/vim-fugitive' })
+-- vim.pack.add({ 'https://github.com/tpope/vim-fugitive' })
+--
+-- vim.api.nvim_set_keymap('n', '<leader>gg', '<cmd>G<CR>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<leader>gd', '<cmd>Git diff<CR>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<leader>gv', '<cmd>Gdiffsplit<CR>', { noremap = true, silent = true })
 
-vim.api.nvim_set_keymap('n', '<leader>gg', '<cmd>G<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>gd', '<cmd>Git diff<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>gv', '<cmd>Gdiffsplit<CR>', { noremap = true, silent = true })
+vim.pack.add({ 'https://github.com/mistweaverco/jujutsu.nvim' })
+
+local jj = require('jujutsu')
+
+jj.setup({
+  kind = 'split'
+})
+
+vim.keymap.set('n', '<leader>jj', function()
+  jj.open()
+end)
+
+vim.keymap.set('n', '<leader>jr', function()
+  jj.refresh()
+end)
