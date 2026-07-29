1660419ed526c1211cfe01ec4a7870170d5624e1
DamascenoV <victor.c.damasceno@gmail.com>
2026-07-29 09:05:45 +0100

    add: jiejie some tests

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,5 +1,7 @@
-vim.pack.add({ 'https://github.com/tpope/vim-fugitive' })
+-- vim.pack.add({ 'https://github.com/tpope/vim-fugitive' })
+--
+-- vim.api.nvim_set_keymap('n', '<leader>gg', '<cmd>G<CR>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<leader>gd', '<cmd>Git diff<CR>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<leader>gv', '<cmd>Gdiffsplit<CR>', { noremap = true, silent = true })
 
-vim.api.nvim_set_keymap('n', '<leader>gg', '<cmd>G<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>gd', '<cmd>Git diff<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>gv', '<cmd>Gdiffsplit<CR>', { noremap = true, silent = true })
+vim.pack.add({ "https://github.com/jceb/jiejie.nvim" })
