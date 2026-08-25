6b42adb0ca1835026178b39fcc36ac8a3a896455
DamascenoV <victor.c.damasceno@gmail.com>
2026-08-25 15:47:30 +0100

    neogit console

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,7 +1,8 @@
 vim.pack.add({ 'https://github.com/NeogitOrg/neogit' })
 
 require('neogit').setup({
-  kind = 'replace'
+  kind = 'replace',
+  auto_close_console = false
 })
 
 vim.api.nvim_set_keymap('n', '<leader>gg', '<cmd>Neogit<CR>', { noremap = true, silent = true })
